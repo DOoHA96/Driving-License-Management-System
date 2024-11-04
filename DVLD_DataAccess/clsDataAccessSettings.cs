5848aa066803b52e637a57216e1c79c46eaf8f0b
Mohamed Mamdouh <momamdouh961@gmail.com>
2024-11-04 14:36:47 +0200

    first commit

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        //public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+        public static string ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+
+    }
+}
